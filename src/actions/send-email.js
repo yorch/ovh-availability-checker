c096b5517c1de8e7b0ee9fe5be9cf3d18eae10a4
Jorge Barnaby <jorge.barnaby@gmail.com>
2020-02-17 23:30:46 -0500

    Replace twilio with simple-sms-sender

--- src/actions/send-email.js
+++ src/actions/send-email.js
@@ -10,35 +10,37 @@ const {
 } = require('../config');
 
 const sendEmail = async ({ content, logger }) => {
-    if (enable) {
-        const transporter = nodemailer.createTransport({
-            host,
-            port,
-            secure,
-            auth: {
-                user,
-                pass
-            }
-        });
-        try {
-            const info = await transporter.sendMail({
-                from: `"${from.name}" <${from.email}>`,
-                to: toEmail,
-                subject,
-                text: content,
-                html: content
-            });
-            const { messageId } = info;
+    if (!enable) {
+        return;
+    }
 
-            logger.info(`Message sent: ${messageId}`);
-            // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-            // Preview only available when sending through an Ethereal account
-            // logger.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
-            // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-        } catch (err) {
-            logger.error(`Could not send the email`, err);
+    const transporter = nodemailer.createTransport({
+        host,
+        port,
+        secure,
+        auth: {
+            user,
+            pass
         }
+    });
+    try {
+        const info = await transporter.sendMail({
+            from: `"${from.name}" <${from.email}>`,
+            to: toEmail,
+            subject,
+            text: content,
+            html: content
+        });
+        const { messageId } = info;
+
+        logger.info(`Message sent: ${messageId}`);
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+        // Preview only available when sending through an Ethereal account
+        // logger.info(`Preview URL: ${nodemailer.getTestMessageUrl(info)}`);
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    } catch (err) {
+        logger.error(`Could not send the email`, err);
     }
 };
 
--- src/actions/send-sms.js
+++ src/actions/send-sms.js
@@ -1,59 +1,30 @@
+import { SmsSender } from 'simple-sms-sender';
+
 const {
     sms: {
         enable,
-        twilio: { accountSid, from, messagingServiceSid, secret, sid },
+        twilio: { accountSid, from, secret, sid },
         toNumber
     }
 } = require('../config');
 
 const sendSms = ({ content, logger }) => {
-    if (enable) {
-        if (!content || content.length === 0) {
-            logger.info('No content to send');
-            return;
-        }
-
-        const recipients = toNumber.split(',');
-
-        if (!toNumber || recipients.length === 0) {
-            logger.info('No recipients to send');
-            return;
-        }
-
-        const client = new twilio(sid, secret, { accountSid });
-
-        const sendSmsToNumber = (phoneNumber) => {
-            logger.info(`Trying to send to number '${phoneNumber}'`);
-            return client.messages
-                .create({
-                    body,
-                    to: phoneNumber.trim(),
-                    from
-                })
-                .then((message) => {
-                    const { errorCode, errorMessage, status, statusCode } = message;
-                    if (errorCode) {
-                        logger.error(
-                            `There was an error sending SMS to number ${phoneNumber} (${errorCode} - ${errorMessage})`
-                        )
-
-                    } else {
-                        logger.info(
-                            `Sent SMS to number '${phoneNumber}' successful (status ${statusCode} - ${status})`
-                        );
-                    }
-                    return message;
-                })
-                .catch((error) => {
-                    logger.error(
-                        `Could not send SMS to number '${phoneNumber}'`,
-                        error
-                    );
-                });
-        };
-
-        return Promise.all(recipients.map(sendSmsToNumber));
+    if (!enable) {
+        return;
     }
+
+    const smsSender = new SmsSender({
+        accountSid,
+        fromNumber: from,
+        logger,
+        secret,
+        sid
+    });
+
+    return smsSender.sendSms({
+        body,
+        recipients: toNumber.split(',')
+    });
 };
 
 module.exports = {
