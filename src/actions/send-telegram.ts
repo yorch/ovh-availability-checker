c4278e93c5035af7ff06af05d51279e161568d77
Jorge Barnaby <jorge.barnaby@gmail.com>
2022-11-06 19:32:55 -0500

    Cleanup

--- src/actions/send-telegram.ts
+++ src/actions/send-telegram.ts
@@ -3,10 +3,6 @@ import TelegramBot from 'node-telegram-bot-api';
 import { telegram } from '../config';
 import type { Action } from '../types';
 
-// eslint-ignore-next-line node/prefer-global/process
-// @ts-expect-error - Avoid error suggesting to use ['NTBA_FIX_319'] which conflicts with autoformat
-process.env.NTBA_FIX_319 = 'false';
-
 const { botToken, enable, chatId } = telegram;
 
 export const sendTelegram: Action = async ({ content, logger }) => {
